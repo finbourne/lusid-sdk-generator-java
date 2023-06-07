5527724c8a192b2b4735c12bb41c1e934e7189d2
Paul Saunders <paul.saunders@finbourne.com>
2023-06-07 15:44:51 +0100

    Fixes group id of locally installed package.
    Signed-off-by: Paul Saunders <paul.saunders@finbourne.com>

--- publish/publish-local.sh
+++ publish/publish-local.sh
@@ -8,7 +8,7 @@ find . -name "*.jar"
 
 mvn org.apache.maven.plugins:maven-install-plugin:3.1.1:install-file  \
   -Dfile=sdk/target/${PACKAGE_NAME}-${PACKAGE_VERSION}.jar \
-  -DgroupId=com.finbourne.${PROJECT_NAME} \
+  -DgroupId=com.finbourne \
   -DartifactId=${PACKAGE_NAME} \
   -Dversion=${PACKAGE_VERSION} \
   -Dpackaging=jar \
