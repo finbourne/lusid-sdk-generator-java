aaf2d669b8615f91991faf09ca2f0610f22b83a8
Paul Saunders <paul.saunders@finbourne.com>
2023-09-29 13:32:33 +0100

    fix(validation): Removes additional property validation
    Signed-off-by: Paul Saunders <paul.saunders@finbourne.com>

--- publish/publish-local.sh
+++ publish/publish-local.sh
@@ -1,17 +1,20 @@
 #!/bin/bash -e
 
-mvn -e -f sdk/pom.xml test-compile compile
+cd sdk
 
-mvn -e -f sdk/pom.xml package
+javadoc=$(which javadoc)
+mvn clean
+mvn test -DjavadocExecutable=${javadoc}
+mvn package -DjavadocExecutable=${javadoc} -Dmaven.test.skip=true
 
 find . -name "*.jar"
 
-mvn org.apache.maven.plugins:maven-install-plugin:3.1.1:install-file  \
-  -Dfile=sdk/target/${PACKAGE_NAME}-${PACKAGE_VERSION}.jar \
+mvn org.apache.maven.plugins:maven-install-plugin:3.1.1:install-file  -X \
+  -Dfile=target/${PACKAGE_NAME}-${PACKAGE_VERSION}.jar \
   -DgroupId=com.finbourne \
   -DartifactId=${PACKAGE_NAME} \
   -Dversion=${PACKAGE_VERSION} \
   -Dpackaging=jar \
-  -DlocalRepositoryPath=/tmp/local-maven-repo
+  -DlocalRepositoryPath=/tmp/local-maven-repo 
 
 ls -tla /tmp/local-maven-repo
