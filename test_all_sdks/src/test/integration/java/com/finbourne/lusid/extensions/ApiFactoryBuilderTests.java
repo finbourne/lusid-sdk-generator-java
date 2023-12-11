1ac3ea9242e300d9f8d603cb0d64174897f5f0fd
Nicole Peters <nicole.peters@finbourne.com>
2023-12-11 12:41:46 +0000

    CTECH-3380: separate tests that can be run for all sdks from tests that can only run for lusid

---
+++ test_all_sdks/src/test/integration/java/com/finbourne/lusid/extensions/ApiFactoryBuilderTests.java
@@ -0,0 +1,50 @@
+package com.finbourne.lusid.extensions;
+
+import com.finbourne.lusid.ApiException;
+//import com.finbourne.lusid.api.InstrumentsApi;
+//import com.finbourne.lusid.model.ResourceListOfInstrumentIdTypeDescriptor;
+import com.finbourne.lusid.extensions.ApiConfigurationException;
+import com.finbourne.lusid.extensions.ApiFactory;
+import com.finbourne.lusid.extensions.ApiFactoryBuilder;
+import com.finbourne.lusid.extensions.auth.FinbourneTokenException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.empty;
+import static org.junit.Assert.assertThat;
+
+public class ApiFactoryBuilderTests {
+
+        @Rule
+        public ExpectedException thrown = ExpectedException.none();
+
+        @Test
+        public void build_WithExistingConfigurationFile_ShouldReturnFactory()
+                        throws ApiException, ApiConfigurationException, FinbourneTokenException {
+                ApiFactory ApiFactory = ApiFactoryBuilder.build(CredentialsSource.credentialsFile);
+                assertThat(ApiFactory, is(notNullValue()));
+                //assertThatFactoryBuiltApiCanMakeLUSIDCalls(ApiFactory);
+        }
+
+        @Test
+        public void build_WithEnvironmentVariables_ShouldReturnFactory()
+                        throws ApiException, ApiConfigurationException, FinbourneTokenException {
+                ApiFactory ApiFactory = ApiFactoryBuilder.build();
+                assertThat(ApiFactory, is(notNullValue()));
+                //assertThatFactoryBuiltApiCanMakeLUSIDCalls(ApiFactory);
+        }
+
+        // private static void assertThatFactoryBuiltApiCanMakeLUSIDCalls(ApiFactory ApiFactory)
+        //                 throws ApiException {
+        //         InstrumentsApi instrumentsApi = ApiFactory.build(InstrumentsApi.class);
+        //         ResourceListOfInstrumentIdTypeDescriptor instrumentIdTypeDescriptor = instrumentsApi
+        //                         .getInstrumentIdentifierTypes().execute();
+        //         assertThat("Instruments API created by factory should have returned instrument identifier types",
+        //                         instrumentIdTypeDescriptor, is(notNullValue()));
+        //         assertThat("Instrument identifier types returned by the Instrument API should not be empty",
+        //                         instrumentIdTypeDescriptor.getValues(), not(empty()));
+        // }
+
+}
---
+++ test_all_sdks/src/test/unit/java/com/finbourne/lusid/extensions/CredentialsSource.java
@@ -0,0 +1,8 @@
+package com.finbourne.lusid.extensions;
+
+/**
+ * Filename containing LUSID API credentials
+ */
+public class CredentialsSource {
+    public static final String credentialsFile = "secrets.json";
+}
